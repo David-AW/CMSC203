f65a161108f4d96c6d86abd96e357baa346cb2ab
Dave W. <davidallenwery@gmail.com>
2024-10-15 16:53:37 -0400

    Add files via upload

---
+++ JUnitLab/src/GradeBookTester.java
@@ -0,0 +1,58 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GradeBookTester {
+
+	GradeBook test1, test2;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		test1 = new GradeBook(5);
+		test2 = new GradeBook(5);
+		
+		test1.addScore(95.6);
+		test1.addScore(54.4);
+		
+		test2.addScore(70.3);
+		test2.addScore(69.9);
+		test2.addScore(22.2);
+		test2.addScore(78.9);
+		test2.addScore(23.4);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		test1 = null;
+		test2 = null;
+	}
+
+	@Test
+	void testAddScore() {
+		assertTrue(test1.toString().equals("95.6 54.4 0.0 0.0 0.0"));
+		assertTrue(test2.toString().equals("70.3 69.9 22.2 78.9 23.4"));
+		assertEquals(2, test1.getScoresSize());
+		assertEquals(5, test2.getScoresSize());
+	}
+
+	@Test
+	void testSum() {
+		assertEquals(150, test1.sum());
+		assertEquals(264.7, test2.sum());
+	}
+
+	@Test
+	void testMinimum() {
+		assertEquals(54.4, test1.minimum());
+		assertEquals(22.2, test2.minimum());
+	}
+
+	@Test
+	void testFinalScore() {
+		assertEquals(95.6, test1.finalScore());
+		assertEquals(242.5, test2.finalScore());
+	}
+
+}
